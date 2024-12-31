342e52f07e2fcf990d30240e9f89772526f7fb84
Raehyeok, Cho <raehyeok.cho@gmail.com>
2024-12-31 12:03:48 +0900

    코멘트 추가

--- src/main/java/me/jjorae/sample_batch_processing/SampleBatchProcessingApplication.java
+++ src/main/java/me/jjorae/sample_batch_processing/SampleBatchProcessingApplication.java
@@ -3,6 +3,9 @@ package me.jjorae.sample_batch_processing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Spring Boot 3.0부터는 @EnableBatchProcessing 어노테이션이 필요하지 않다.
+ */
 @SpringBootApplication
 public class SampleBatchProcessingApplication {
 
